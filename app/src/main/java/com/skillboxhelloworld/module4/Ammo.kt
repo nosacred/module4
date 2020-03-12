bbaf9ee50cc067f17e8de3a610cff398aaef23c6
NoSacred <und3rrrr@gmail.com>
2020-03-12 15:25:19 +0300

    Ammo add

---
+++ app/src/main/java/com/skillboxhelloworld/module4/Ammo.kt
@@ -0,0 +1,21 @@
+package com.skillboxhelloworld.module4
+
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+enum class Ammo (
+    private val damage:Int,
+    private val criticalChance:Int,
+    private val criticalMult:Int
+){
+    BulletPistol(10,3,2),
+    BulletGun(25,5,3),
+    BulletRifle(40,10,5);
+
+    fun damageDone():Int{
+        var dmg = damage
+        if (Random.nextInt(0..criticalChance) == criticalChance)  dmg = damage*criticalMult
+        return dmg
+    }
+
+}
---
+++ app/src/main/java/com/skillboxhelloworld/module4/Main.kt
@@ -0,0 +1,8 @@
+package com.skillboxhelloworld.module4
+
+fun main() {
+    println(Ammo.BulletPistol.damageDone())
+    println(Ammo.BulletGun.damageDone())
+    println(Ammo.BulletRifle.damageDone())
+
+}
